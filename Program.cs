f83e114ec7a31a3192494ecae474b602319bc1ec
Yung-Modi <chrisreaper404@gmail.com>
2025-02-25 14:54:04 -0600

    code

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Welcomes the user
+        Console.WriteLine("Welcome!");
+
+        // Asks the user for their first name and assign it to the variable "firstName"
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        // Asks the user for their last name and assign it to the variable "lastName"
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        // Prints a greeting message with the user's first and last name
+        Console.WriteLine($"Hello, {firstName} {lastName}!");
+    }
+}
